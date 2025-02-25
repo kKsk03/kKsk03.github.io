859baecc1225e29a64c34931578b9228623486df
kKsk <alonzomax1210@gmail.com>
2025-02-25 21:00:28 +0800

    vsnasm

--- docs/.vitepress/sidebar.ts
+++ docs/.vitepress/sidebar.ts
@@ -3,7 +3,8 @@ export const sideBar = {
         {
             text: "常用工具",
             items: [
-                { text: "Windows 激活", link: "/common/tools/windows-activate" }
+                { text: "Windows 激活", link: "/common/tools/windows-activate" },
+                { text: "VS NASM 安装", link: "/common/tools/vs_nasm_install" }
             ]
         },
         {
