7b8539265f0dc540b732bca0154d1191660f3b9d
kKsk <alonzomax1210@gmail.com>
2025-08-21 01:03:07 +0800

    update

--- docs/.vuepress/navbar.ts
+++ docs/.vuepress/navbar.ts
@@ -7,12 +7,13 @@
 import { defineNavbarConfig } from 'vuepress-theme-plume'
 
 export default defineNavbarConfig([
-    { text: '首页', link: '/' },
-    { text: '博客', link: '/blog/' },
-    { text: '标签', link: '/blog/tags/' },
-    { text: '归档', link: '/blog/archives/' },
+    { text: '首页', link: '/', icon: 'material-symbols:home-outline' },
+    { text: '博客', link: '/blog/', icon: 'material-symbols:article-outline' },
+    { text: '标签', link: '/blog/tags/', icon: 'mdi:tag-outline' },
+    { text: '归档', link: '/blog/archives/', icon: 'material-symbols:archive-outline' },
     {
         text: '笔记',
+        icon: 'material-symbols:ink-pen-outline',
         items: [{ text: '示例', link: '/notes/demo/README.md' }]
     },
 ])
---
+++ docs/tools/windows-activate.md
@@ -0,0 +1,32 @@
+---
+title: Windows 激活
+createTime: 2025/08/21 00:50:30
+permalink: /article/fvpinnr2/
+---
+
+# Windows 激活
+
+使用 [massgrave](https://massgrave.dev/) 提供的激活方法进行激活
+
+:::: steps
+
+1. 以管理员方式打开终端（非CMD），输入代码：
+
+    ```sh
+    irm "https://get.activated.win" | iex
+    ```
+
+    ::: tip 备选方式
+
+    ```sh
+    irm "https://massgrave.dev/get" | iex
+    ```
+
+    :::
+
+2. 将提示激活的选项
+
+    选择 `[1] HWID for Windows activation`  
+    然后将自动激活 Windows。
+
+::::
